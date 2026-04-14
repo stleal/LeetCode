7781f2bbd1fd2ae924e186c365dbaede883f6145
Samuel Teodoro Leal <Leal.Samir@Outlook.com>
2026-04-14 08:31:28 -0400

    refactoring ScoreOfString method

--- Easy/EasySolutions/Solutions/ScoreOfStringSolution.cs
+++ Easy/EasySolutions/Solutions/ScoreOfStringSolution.cs
@@ -2,11 +2,10 @@ public class ScoreOfStringSolution
 {
   public int ScoreOfString(string s)
   {
-    int score = 0;
-    int absDifference = 0;
+    var score = 0;
     for (int i = 0; i < s.Length-1; i++)
     {
-      absDifference = Math.Abs(s[i] - s[i+1]);
+      var absDifference = Math.Abs(s[i] - s[i+1]);
       score += absDifference;
     }
     return score;
