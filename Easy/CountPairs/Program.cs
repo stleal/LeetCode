6ccd0f5e18c1261f6e4acfe37895e8c3cf8c72eb
Samuel Teodoro Leal <Leal.Samir@Outlook.com>
2026-08-29 21:04:38 -0400

    adding a solution for count pairs in an array problem

---
+++ Easy/CountPairs/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Program
+{
+  public static void Main(string[] args)
+  {
+    int target = 2;
+    int[] nums = { -1, 1, 2, 3, 1};
+    var result = CountPairs(nums, target);
+    Console.WriteLine(result);
+  }
+
+  public static int CountPairs(int[] nums, int target)
+  {
+    int count = 0;
+    for (int i = 0; i < nums.Length; i++)
+    {
+      for (int j = i + 1; j < nums.Length; j++)
+      {
+        if (nums[i] + nums[j] < target)
+        {
+          count++;
+        }
+      }
+    }
+    return count;
+  }
+}
