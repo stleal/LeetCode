0293711ac90c7c5c5d7c0cfac553ecc832ebf406
Samuel Teodoro Leal <Leal.Samir@Outlook.com>
2026-09-05 18:00:01 -0400

    Adding a solution for MinElement

---
+++ Easy/MinElement/Program.cs
@@ -0,0 +1,28 @@
+﻿public class Program {
+  public static void Main(string[] args)
+  {
+    var answer = MinElement(new int[] { 10, 12, 13, 14 });
+    Console.WriteLine(answer);
+  }
+
+  public static int MinElement(int[] nums) {
+      int[] sumOfDigits = new int[nums.Length];
+      for (int i = 0; i < nums.Length; i++)
+      {
+          int x = nums[i];
+          int sum = 0;
+          while (x > 0)
+          {
+              sum += x % 10;
+              x /= 10;
+          }
+          sumOfDigits[i] = sum;
+      }
+      int min = sumOfDigits[0];
+      for (int i = 1; i < sumOfDigits.Length; i++)
+      {
+          min = (sumOfDigits[i] < min) ? sumOfDigits[i] : min;
+      }
+      return min;
+  }
+}
