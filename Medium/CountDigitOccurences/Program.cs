63afa256b90540d51ab392bbfe6907fae6d29878
Samuel Teodoro Leal <Leal.Samir@Outlook.com>
2026-09-06 16:58:35 -0400

    Adding a solution for CountDigitOccurences

---
+++ Medium/CountDigitOccurences/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+      int[] nums = { 12, 54, 32, 22 };
+      int digit = 2;
+      Program p = new Program();
+      int result = CountDigitOccurrences(nums, digit);
+      Console.WriteLine(result);
+    }
+    public static int CountDigitOccurrences(int[] nums, int digit) {
+        int count = 0;
+        foreach (var num in nums)
+        {
+            int x = num;
+            while (x > 0)
+            {
+                var q = x%10;
+                if (q == digit)
+                    count++;
+                x/=10;
+            }
+        }
+        return count;
+    }
+}
