327aadc9c3f8d133cbf0908799fc448dbcd24ea8
Samuel Teodoro Leal <Leal.Samir@Outlook.com>
2026-09-06 10:26:11 -0400

    Adding optimized GCD insertion method and optimized GCD calculation

--- Easy/InsertGreatestCommonDivisors/Program.cs
+++ Easy/InsertGreatestCommonDivisors/Program.cs
@@ -36,15 +36,41 @@ public class Program {
       return head;
   }
 
+  public static ListNode InsertGreatestCommonDivisorsOptimized(ListNode head) {
+    if (head == null || head.next == null)
+      return head;
+    var cursor = head;
+    while (cursor.next != null)
+    {
+      var next = cursor.next;
+      cursor.next = new ListNode(
+        FindGreatestCommonDenominatorOptimized(cursor.val, next.val),
+        next);
+      cursor = next;
+    }
+    return head;
+  }
+
   public static int FindGreatestCommonDenominator(int x, int y)
   {
-      var gcd = 0;
-      var smallest = (x < y) ? x : y;
-      var largest = (x > y) ? x : y;
-      for (int i = 1; i <= smallest; i++)
-      {
-          gcd = ((largest % i == 0) && (smallest % i == 0))? i : gcd;
-      }
-      return gcd;
+    var gcd = 0;
+    var smallest = (x < y) ? x : y;
+    var largest = (x > y) ? x : y;
+    for (int i = 1; i <= smallest; i++)
+    {
+        gcd = ((largest % i == 0) && (smallest % i == 0))? i : gcd;
+    }
+    return gcd;
+  }
+
+  public static int FindGreatestCommonDenominatorOptimized(int x, int y)
+  {
+    x = Math.Abs(x);
+    y = Math.Abs(y);
+    while (y != 0)
+    {
+      (x, y) = (y, x % y);
+    }
+    return x;
   }
 }
