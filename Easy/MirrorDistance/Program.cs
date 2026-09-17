72fe3fa836cf3a6ce7d37e490ad203c8045db430
Samuel Teodoro Leal <Leal.Samir@Outlook.com>
2026-09-16 22:24:42 -0400

    adding a solution for MirrorDistance

---
+++ Easy/MirrorDistance/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Program
+{
+  public static void Main(string[] args)
+  {
+    var result = MirrorDistance(25);
+    Console.WriteLine("The mirror distance is: " + result);
+  }
+  public static int MirrorDistance(int n)
+  {
+    return Math.Abs(n - Reverse(n));
+  }
+  public static int Reverse(int n)
+  {
+    string reversed = "";
+    while (n > 0)
+    {
+      int digit = n % 10;
+      reversed += digit.ToString();
+      n /= 10;
+    }
+    return Convert.ToInt32(reversed);
+  }
+}
